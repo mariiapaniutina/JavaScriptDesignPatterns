5a4f780791971657668a3837d81b3e0049dbd9f6
Mariia Paniutina <yangol89@gmail.com>
2016-04-02 20:47:20 -0700

    Update Stack.js

--- dataStructure/Stack.js
+++ dataStructure/Stack.js
@@ -1,21 +1,20 @@
 var Stack = function(){
   this.size = 0;
-  this.arr = [];
+  this.data = [];
 };
 
 Stack.prototype.push = function(value){
-  this.arr[this.size] = value;
+  this.data[this.size] = value;
   this.size++;
 };
 
 Stack.prototype.pop = function(){
-   var el = this.arr[this.size]
+  var el = this.data[this.size];
   if(this.size > 0){
     this.size--;
-    this.arr.length = this.size;
+    this.data.length = this.size;
   }else {
-    this.size = 0;
-    this.arr = [];
+    this.data = [];
   }
   return el;
 };
