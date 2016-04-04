9f270e1c4f4a5657f7123c226161161967b33767
Mariia Paniutina <yangol89@gmail.com>
2016-04-03 21:19:18 -0700

    Create LinkedList.js

---
+++ dataStructure/LinkedList.js
@@ -0,0 +1,43 @@
+var LinkedList = function(){
+  this.Node = function(val){
+    this.val = val;
+    this.next = null;
+  };
+  
+  this.head = null;
+  this.size = 0;
+};
+
+LinkedList.prototype.push = function(val){
+  var el = new this.Node(val);
+  if(this.head == null){
+    this.head = el;
+  } else {
+    var newHead = this.head;
+    while(newHead.next !== null){
+      newHead = newHead.next;
+    }
+    newHead.next = el;
+  }
+  
+  this.size++;
+};
+
+LinkedList.prototype.pop = function(){
+  var newHead = this.head;
+  if(this.size < 2){
+    this.head = null;
+  }
+  for (var i=0; i<this.size-2; i++){
+    newHead = newHead.next;
+  }
+  newHead.next = null;
+};
+
+LinkedList.prototype.getSize = function(){
+  return this.size;
+};
+
+LinkedList.prototype.getData = function(){
+  return JSON.stringify(this.head);
+};
