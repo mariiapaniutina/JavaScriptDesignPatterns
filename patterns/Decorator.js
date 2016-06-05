6309e53b85578e968db2bc78f94f023c094b5e07
Mariia Paniutina <yangol89@gmail.com>
2016-06-04 21:03:14 -0700

    Update Decorator.js

--- patterns/Decorator.js
+++ patterns/Decorator.js
@@ -39,3 +39,51 @@ console.time('third call');
 fib(40);
 console.timeEnd('third call');
 //third call: 0.04ms
+
+
+/*
+One more example is debounce function.
+On current example, if callback has not been called at least once within an timeer, 
+it will call the latest one
+*/
+var sayLetter = function(letter){
+	return console.log('I think this is letter ' + letter);
+};
+
+var helper = {};
+helper.debouncer = function(fn, timer){
+  var time = new Date().getTime();
+  var stack = [];
+  var fnCalled = false;
+  
+	return function(){
+		var args = arguments;
+    var context = this;
+    var currTime = new Date().getTime();
+    stack.push(args);
+    
+    if ((currTime - time >= timer)){
+      time = currTime;
+      fnCalled = true;
+      return fn.apply(context, args);
+    }
+    
+    setTimeout(function(){
+      if (fnCalled === false){
+        fnCalled = true;
+        return fn.apply(context, stack[stack.length-1]);
+      }
+    }, timer);
+    
+	};
+};
+
+var testIt = helper.debouncer(sayLetter, 100);
+
+testIt('A');
+testIt('B');
+testIt('C');
+testIt('D');
+testIt('F');
+
+//I think this is letter F
